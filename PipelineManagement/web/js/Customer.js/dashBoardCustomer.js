714909a56a7c45d112b080fbd90a5c910eb62961
William0832 <squall0832@gmail.com>
2021-03-18 18:15:16 +0800

    modify dashBoard_Customer page

--- PipelineManagement/web/js/Customer.js/dashBoardCustomer.js
+++ PipelineManagement/web/js/Customer.js/dashBoardCustomer.js
@@ -2,43 +2,13 @@
   let data = []
   const tbody = $('#custom-table__content')
 
-  const showCustomer = (targets) => {
+  const showCustomer = ({ targets }) => {
     if(targets.length > 0) {
       tbody.empty()
       const inputCols = ['customer', 'testing', 'markpoint', 'stamp', 'package', 'contact']
       $.each(targets, (i, e) => {
         const { customer_id } = e
         const row = $('<tr>')
-        row.append($('<a>').attr("href", `/pages/Dashboard_Customer/${customer_id}`).attr("target", "_blank")
-          .append($('<span>')
-            .addClass("glyphicon glyphicon-edit")
-            .css("margin-top", "3px")))
-
-        inputCols.forEach(col => {
-          let text = e[col]
-            ? e[col]
-            : '-'
-          row.append($('<td>').text(text))
-        })
-        tbody.append(row)
-      })
-      return
-    }
-    tbody.empty()
-  }
-
-  $.ajax({
-    url: '/customers',
-    type: 'GET',
-    dataType: 'json',
-    success (json) {
-      const inputCols = ['customer', 'testing','markpoint','stamp','package','contact']
-      data = json.results
-      $.each(json.results, (i, e) => {
-        // console.log(i,e)
-        const { customer_id } = e
-        const row = $('<tr>')
-        // row.attr('class', 'text-center')
         const btn = $('<a>')
           .attr("href", `/pages/Dashboard_Customer/${customer_id}`)
           .attr("target", "_blank")
@@ -48,7 +18,7 @@
           $('<td>').append(btn)
         )
         inputCols.forEach(col => {
-          const text = e[col] 
+          const text = e[col]
             ? e[col]
             : '-'
           row.append(
@@ -58,6 +28,16 @@
         })
         tbody.append(row)
       })
+    }
+  }
+
+  $.ajax({
+    url: '/customers',
+    type: 'GET',
+    dataType: 'json',
+    success (json) {
+      data = json.results
+      showCustomer({targets: data})
     },
     fail (json) { console.log("fail") }
   })
@@ -69,6 +49,6 @@
       e.customer &&
       e.customer.toLowerCase().includes(str)
     )
-    showCustomer(targets)
+    showCustomer( { targets })
   })
 })()
--- PipelineManagement/web/js/Production/getProductionKanban.js
+++ PipelineManagement/web/js/Production/getProductionKanban.js
@@ -1,28 +1,12 @@
-$(function () {
+;$(function () {
     console.log("Get Putline")
-
-    // let urlParams = new URLSearchParams(window.location.search);
-    // tam_alias = urlParams.get('tam_alias');
-
-
-    
-
     $.ajax({
-        // url: '/getServiceQuota/ec2',
         url: '/workitem/getWorkitems',
         type: 'GET',
         dataType: 'json',
         success: function (json) {
-            // console.log("Get Customer List Table")
-            console.log(json);
-
-            // var total_count = 0;
-            // var complete_survey_count = 0;
-            // var complete_comment_count = 0;
 
             $.each(json.results, function (i, result) {
-
-                // 
                 var test_type = result.test_type;
                 var date = result.create_dt;
                 var category = result.test_style;
@@ -32,44 +16,34 @@ $(function () {
                 var units = result.units;
                 var process = result.process;
                 var status = result.status;
-                var remark = result.remark;
+                // var remark = result.remark;
+                // var scene_class = "label label-default"; 
 
+                var row = $('<tr>')
+                row.append(
+                    $('<a>')
+                    .attr("href", "/pages/workitem.html")
+                    .attr("target", "_blank").append(
+                        $('<span>')
+                        .addClass("glyphicon glyphicon-edit").
+                        css("margin-top","3px")
+                    )
+                )               
 
-                
-
-                var scene_class = "label label-default"; 
-                
-
-
-                var row = $('<tr>');
-
-                row.append($('<a>').attr("href", "/pages/workitem.html").attr("target", "_blank").append($('<span>').addClass("glyphicon glyphicon-edit").css("margin-top","3px")));                
-
-                row.append($('<td>').text(test_type));
-                row.append($('<td>').text(date));
-                row.append($('<td>').text(category));
-                row.append($('<td>').text(customer));
-                row.append($('<td>').text(part_no));
-                row.append($('<td>').text(lot_no));
-                row.append($('<td>').text(units));
-                row.append($('<td>').text(process));
-                row.append($('<td>').text(status));
-                
-                
-                
-
-                
-            
-                $('#productionKanbanTable').find('tbody').append(row);
-
-
-            });
-
-            // document.getElementById("loader-sq").style.display = "none";
-
+                row.append($('<td>').text(test_type))
+                row.append($('<td>').text(date))
+                row.append($('<td>').text(category))
+                row.append($('<td>').text(customer))
+                row.append($('<td>').text(part_no))
+                row.append($('<td>').text(lot_no))
+                row.append($('<td>').text(units))
+                row.append($('<td>').text(process))
+                row.append($('<td>').text(status))
+                $('#productionKanbanTable').find('tbody').append(row)
+            })
         },
-        fail: function (json) { console.log("fail"); }
-    });
-});
+        fail: function (json) { console.log("fail")}
+    })
+})
 
 
